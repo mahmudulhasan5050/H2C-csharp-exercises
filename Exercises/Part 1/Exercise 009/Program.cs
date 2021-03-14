39ea56464399fcca400a08e6f5af3d8ef6539023
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-15 00:50:11 +0200

    15.3.2021

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_09
         public static void Main(string[] args)
         {
             // Write your code here:
-
+            Console.WriteLine("Hello, how are you?");
+            Console.Write("> ");
+            Console.ReadLine();
+            Console.WriteLine("That's interesting, tell me more");
+            Console.Write("> ");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for sharing!");
+            Console.ReadKey();
         }
     }
 }
