a762ebbe9e1e2a5e33160e33ca538c60d0ceb74b
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-14 21:41:11 +0200

    14.3.2021

--- Exercises/Part 1/Exercise 001/Program.cs
+++ Exercises/Part 1/Exercise 001/Program.cs
@@ -7,7 +7,7 @@ namespace exercise_01
         public static void Main(string[] args)
         {
            
-        Console.WriteLine("hello world");
+        Console.WriteLine("Hello World");
 
             Console.ReadKey();
 
