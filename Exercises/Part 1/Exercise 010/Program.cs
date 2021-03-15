2987cf27a2115c9583fb38f70174ec95b65cacfb
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-15 02:21:00 +0200

    15.3.2021

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -7,8 +7,20 @@ namespace exercise_10
         public static void Main(string[] args)
         {
           
+           Console.Write("I will tell a story, but I need some information.\n"+
+                "Give a name for main character:\n" + "> ");
+            string name = Console.ReadLine();
+            Console.Write("Give the character a profession:\n" +"> ");
+            string profession = Console.ReadLine();
 
+            Console.Write("Here is the story:\n"+
+                "Once upon a time there was a "+ profession +" called "+ name +
+                "\nOn her way to work, "+ name + " often poundered what being "+ profession + 
+                " meant to them.\n"+
+                "When you work as a "+ profession +" you meet interesting people.\n"
+                + name +" enjoys their work as "+ profession +", The end.");
 
+            Console.ReadKey();
         }
     }
 }
