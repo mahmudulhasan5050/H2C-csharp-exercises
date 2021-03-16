4e03c76621ee635f9e2e8eba64793c7b02dd4e65
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-16 23:22:19 +0200

    16.3.2021

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,31 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give a String:");
+            Console.Write("> ");
+            string stringValue = Console.ReadLine();
+
+            Console.WriteLine("Give an integer:");
+            Console.Write("> ");
+            string stringIntValue = Console.ReadLine();
+            int intValue = Convert.ToInt32(stringIntValue);
+
+            Console.WriteLine("Give a double:");
+            Console.Write("> ");
+            string stringDoubleValue = Console.ReadLine();
+            double doubleValue = Convert.ToDouble(stringDoubleValue);
+
+            Console.WriteLine("Give a boolean:");
+            Console.Write("> ");
+            string stringBoolValue = Console.ReadLine();
+            bool boolValue = System.Convert.ToBoolean(stringBoolValue);
+
+            Console.WriteLine("Your string: "+ stringValue);
+            Console.WriteLine("Your integer: "+ intValue);
+            Console.WriteLine("Your double: "+ doubleValue);
+            Console.WriteLine("Your boolean: "+ boolValue);
+
+            Console.ReadKey();
 
     }
   }
