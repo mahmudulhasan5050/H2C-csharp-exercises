3773874dc94ef184d297c29c4739926754489991
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-18 12:03:08 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.Write("Give the first number!\n> ");
+            string firstInput = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstInput);
+
+            Console.Write("Give the second number!\n> ");
+            string secondInput = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondInput);
+
+            double average = (firstNumber+secondNumber)/2.0;
+
+            Console.WriteLine("The average is " + average);
+            Console.ReadKey();
 
     }
   }
