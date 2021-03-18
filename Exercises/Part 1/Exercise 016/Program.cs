df348dd39cc4c4e359db927ae18b81fabd158fcf
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-18 10:55:06 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_16
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+       Console.Write("How many days?\n> ");
+            string userInput = Console.ReadLine();
+            int daysAmount = Convert.ToInt32(userInput);
+
+            int daysInSeconds = ((daysAmount*24)*60)*60;
+
+            Console.WriteLine(daysInSeconds);
+            Console.ReadKey();
 
     }
   }
