853894a2dfb901acfeb0dbfa0994271b6798163d
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-18 15:59:50 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,26 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.Write("Give the first number!\n> ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Give the second number!\n> ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("The larger number is " + firstNumber +"!");
+            }
+            else if(firstNumber < secondNumber)
+            {
+                Console.WriteLine("The larger number is " + secondNumber +"!");            
+            }
+            else
+            {
+            Console.WriteLine("They are equal!");
+            }
+            Console.ReadKey();
 
     }
   }
