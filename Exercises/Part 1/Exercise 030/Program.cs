63bb37a8bcf605491c929bc9a16f1c1b1561863f
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-18 16:23:26 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,7 +6,43 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.Write("Give your percent [0 - 100]:\n> ");
+            int number = Convert.ToInt32(Console.ReadLine());
+        
+            if (number < 0)
+            {
+                Console.WriteLine("Impossible");
+            }
+            else if(number <= 49)
+            {
+                Console.WriteLine("Fail");            
+            }
+            else if(number <= 59)
+            {
+                Console.WriteLine("Grade: 1");            
+            }
+            else if(number <= 69)
+            {
+                Console.WriteLine("Grade: 2");            
+            }
+            else if(number <= 79)
+            {
+                Console.WriteLine("Grade: 3");            
+            }
+            else if(number <= 89)
+            {
+                Console.WriteLine("Grade: 4");            
+            }
+            else if(number <= 100)
+            {
+                Console.WriteLine("Grade: 5");  
+            }          
+         
+            else
+            {
+            Console.WriteLine("Outstanding!");
+            }
+            Console.ReadKey();
 
     }
   }
