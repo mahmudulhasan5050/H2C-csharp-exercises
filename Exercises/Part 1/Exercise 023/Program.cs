10866bd286b6ee43e330d9abcc8436caba4fb355
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-18 13:10:49 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.Write("Give the first number!\n> ");
+            string firstInput = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstInput);
+
+            Console.Write("Give the second number!\n> ");
+            string secondInput = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondInput);
+
+            int add = firstNumber + secondNumber; 
+            int deduct = firstNumber - secondNumber;
+            int multiply = firstNumber * secondNumber;
+            double divide = (double)firstNumber/secondNumber;
+
+            Console.WriteLine(firstNumber+" + "+secondNumber+" = "+add);
+            Console.WriteLine(firstNumber+" - "+secondNumber+" = "+deduct);
+            Console.WriteLine(firstNumber+" * "+secondNumber+" = "+multiply);
+            Console.WriteLine(firstNumber+" / "+secondNumber+" = "+divide);
+            Console.ReadKey();
 
     }
   }
