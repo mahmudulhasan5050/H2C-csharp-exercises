ac0d6d7ffbc2054d4e210f16927ade5ea1a117d2
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-19 00:52:01 +0200

    19.3.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,7 +6,28 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            while (true) {
+                
+            Console.Write("Give a number:\n> ");
+            int  answer = Convert.ToInt32(Console.ReadLine());
+            int secondPower = 0;
+
+            if(answer == 0)
+            {
+                break;
+            }
+            else if(answer > 0)
+            {
+                    secondPower = answer * answer;
+                    Console.WriteLine(secondPower);
+            }
+            else
+            {
+                    Console.WriteLine("That is negative");
+            }
+         
+            }
+            Console.ReadKey();
 
     }
   }
