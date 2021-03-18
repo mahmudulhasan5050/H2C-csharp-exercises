4c0fad8e910f561ae781f56567ca249f0b9d18a6
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-18 12:32:00 +0200

    18.3.2021

--- Exercises/Part 1/Exercise 022/Program.cs
+++ Exercises/Part 1/Exercise 022/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_22
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.Write("Give the first number!\n> ");
+            string firstInput = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstInput);
+
+            Console.Write("Give the second number!\n> ");
+            string secondInput = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondInput);
+
+            Console.Write("Give the third number!\n> ");
+            string thirdInput = Console.ReadLine();
+            int thirdNumber = Convert.ToInt32(thirdInput);
+
+            double averageNormal = (firstNumber + secondNumber + thirdNumber)/3.0;
+            string averageFormat = string.Format("{0:0.0}", averageNormal);
+
+            Console.WriteLine("The average is " + averageFormat);
+            Console.ReadKey();
 
     }
   }
