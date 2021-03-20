13413337261c0476b0e862ca7e5805956d9d288a
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-20 18:37:28 +0200

    20.3.2021

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,46 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+      int sum = 0;
+            int count = 0;
+            double average = 0;
+            int evenCount = 0;
+            int oddCount = 0;
 
-      // Write your code here:
- 
+      Console.WriteLine("Give numbers:");
+
+            while (true) {
+                Console.Write("> ");
+                int numbers = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers == -1)
+                {
+                    Console.WriteLine("Thx! Bye!");
+                    break;
+                }
+                else
+                {
+                    if(numbers%2 == 0)
+                    {
+                        evenCount = evenCount + 1;
+                    }
+                    else
+                    {
+                        oddCount = oddCount + 1;
+                    }
+                sum = sum + numbers;
+                count = count + 1;                  
+                 continue;
+                }
+                 
+            }
+            average = sum/(double)count;
+            Console.WriteLine("Sum: "+ sum);
+            Console.WriteLine("Numbers: "+ count);
+            Console.WriteLine("Average: "+ average);
+            Console.WriteLine("Even: "+ evenCount);
+            Console.WriteLine("Odd: "+ oddCount);
+            Console.ReadKey();
     }
   }
 }
