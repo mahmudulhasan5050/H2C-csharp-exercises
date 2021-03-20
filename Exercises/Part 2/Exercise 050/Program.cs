42844ad0701e7b38cb0fdbd733c8cc1f9c17e139
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-20 18:55:01 +0200

    20.3.2021

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -6,13 +6,22 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
- 
+            Console.Write("How many times?\n> ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            // Call your method here:
+            PrintPhrase(userInput);
+            Console.ReadKey();
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase(int times) {
+          
+            for(int i = 0; i < times; i++)
+            {
+                Console.WriteLine("In a hole in the ground there lived a method");
+            }
     }
   }
 }
