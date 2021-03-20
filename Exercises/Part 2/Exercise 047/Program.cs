aba772a926e5ba23b32353d3fefaf2dfa3aa9479
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-20 15:25:09 +0200

    20.3.2021

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -6,8 +6,29 @@ namespace exercise_47
   {
     public static void Main(string[] args)
     {
+            //section 1
+            Console.Write("Where to?\n> ");
+            int userInputSection1 = Convert.ToInt32(Console.ReadLine());
 
-      // Write your code here:
+            for(int i = 1; i <= userInputSection1; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //section 2 and 3
+            Console.Write("Where to?\n> ");
+            int userInputSection2To = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Where from?\n> ");
+            int userInputSection2From = Convert.ToInt32(Console.ReadLine());
+           
+            if(userInputSection2To > userInputSection2From) {              
+            for(int j = userInputSection2From; j <= userInputSection2To; j++)
+            {
+                Console.WriteLine(j);
+            }
+            }
+            
+            Console.ReadKey(); 
       
     }
   }
