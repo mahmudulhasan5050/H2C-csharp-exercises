9c5d367ec203fb5d2a976a8a1e7a96f3f192a453
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-22 00:28:05 +0200

    22.3.2021

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -7,10 +7,17 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      PrintFromNumberToOne(9);
+            Console.ReadKey();
     }
 
     // Write your method here:
-
+            public static void PrintFromNumberToOne(int number) {
+          
+            for(int i = number; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            }
   }
 }
