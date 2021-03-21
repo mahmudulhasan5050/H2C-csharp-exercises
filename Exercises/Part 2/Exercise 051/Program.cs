b38276742b67c84c1830311b5fe9c0422078d3d9
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-22 00:23:03 +0200

    22.3.2021

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -7,11 +7,17 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+      PrintUnitNumber(9);
+      Console.ReadKey();
     }
 
     // Write your method here:
-
+        public static void PrintUnitNumber(int number) {
+          
+            for(int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
   }
 }
