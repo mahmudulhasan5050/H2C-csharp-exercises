f0d780f635c59d836798786e5df6a330d16eb647
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-25 00:44:13 +0200

    25.3.2021

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -7,10 +7,20 @@ namespace exercise_54
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      DivisibleByThreeInRange(3,6);
+      Console.ReadKey();
     }
 
     // Write your method here:
-
+     public static void DivisibleByThreeInRange(int beginning, int end)
+        {
+           for(int i = beginning; i <= end; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
   }
 }
