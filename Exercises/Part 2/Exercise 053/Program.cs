c60fc1c4dfd12bcad424267675309345184913db
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-24 21:41:18 +0200

    24.3.2021

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -7,11 +7,16 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
+            Division(7,3);
+            Console.ReadKey();
 
     }
 
     // Write your method here:
-
+    public static void Division(int numerator, int denominator)
+        {
+            double result = numerator/(double)denominator;
+            Console.WriteLine(result);
+        }
   }
 }
