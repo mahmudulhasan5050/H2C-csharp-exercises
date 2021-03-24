528300b9786b0b1a93d5c5e5b418102f34e58848
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-25 01:11:11 +0200

    25.3.2021

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -8,9 +8,26 @@ namespace exercise_59
     {
       int answer = Greatest(2, 7, 3);
       Console.WriteLine("Greatest: " + answer);
+     Console.ReadKey();
     }
 
     // Write your method here:
-
+    public static int Greatest(int number1, int number2, int number3)
+        {
+            int greatest = 0;
+            if(number1 > number2 && number1 > number3)
+            {
+                greatest = number1;
+            }
+            else if(number2 > number1 && number2 > number3)
+            {
+                greatest = number2;
+            }
+            else
+            {
+                greatest = number3;
+            }
+            return greatest;
+        }
   }
 }
