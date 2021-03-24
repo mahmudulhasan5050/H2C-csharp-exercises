a320fceb756b0496811e7a262554701a2b665aa2
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-25 01:03:05 +0200

    25.3.2021

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -8,9 +8,22 @@ namespace exercise_58
     {
       int answer = Smallest(2, 7);
       Console.WriteLine("Smallest: " + answer);
+      Console.ReadKey();
     }
 
     // Write your method here:
-    
+    public static int Smallest(int number1, int number2)
+        {
+            int smallest = 0;
+            if(number1 < number2)
+            {
+                smallest = number1;
+            }
+            else
+            {
+                smallest = number2;
+            }
+            return smallest;
+        }
   }
 }
