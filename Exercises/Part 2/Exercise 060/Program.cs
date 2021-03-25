3d1f8ae81f18daebfc3efd78bc9e71bc0bcfefc8
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-25 12:12:35 +0200

    25.3.2021

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -6,7 +6,23 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
+         PrintStars(5);
+         PrintStars(3);
+         PrintStars(9);
 
+         Console.WriteLine("");
+
+         PrintSquare(4);
+         
+         Console.WriteLine("");
+         
+         PrintRectangle(17,3);
+         
+         Console.WriteLine("");
+        
+         PrintTriangle(4);
+            
+         Console.ReadKey();
     }
 
 
@@ -17,21 +33,49 @@ namespace exercise_60
       // call the print command n times
       // in the end print a line break with the comand
       // Console.WriteLine("");
+      for(int i = 0; i < number; i++)
+            {
+                Console.Write("*");
+            }
+               Console.WriteLine("");
     }
 
     public static void PrintSquare(int size)
     {
-
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
     }
 
     public static void PrintRectangle(int width, int height)
     {
-
+              for(int i = 0; i < height; i++)
+            {
+                for(int j = 0; j < width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
     }
 
     public static void PrintTriangle(int size)
     {
-
+          for(int i = 0; i < size; i++)
+            {
+                
+                for(int j = 0; j <= i; j++)
+                {
+                Console.Write("*");    
+                }
+                Console.WriteLine(" ");
+                
+            }
     }
   }
 }
