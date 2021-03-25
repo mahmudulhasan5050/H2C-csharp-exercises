e37d8f371f9412e6e6fd54708a09dcc87ef4cce7
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-25 14:37:09 +0200

    25.3.2021

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,26 +6,61 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
+            PrintStars(5);
+            PrintSpaces(5);
 
+            Console.WriteLine("");
+
+            PrintRightTriangle(4);
+
+            Console.WriteLine("");
+            ChristmasTree(4);
+            Console.ReadKey();
     }
 
     public static void PrintStars(int number)
     {
-
+  
+           for(int i = 0; i < number; i++)
+            {
+                Console.Write("*");
+            }
+              
     }
 
     public static void PrintSpaces(int number)
     {
-
+           for(int i = 0; i < number; i++)
+            {
+                Console.Write(" ");
+            }
     }
 
     public static void PrintRightTriangle(int size)
     {
+           
+              for(int i = 0; i < size; i++) { 
 
+                for(int j = 0; j < size - i; j++)
+                {
+                Console.Write(" "); 
+                 
+                }
+                for(int k = 0; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+              
+             }   
+            
     }
 
     public static void ChristmasTree(int height)
     {
+            
+  
+
 
     }
   }
