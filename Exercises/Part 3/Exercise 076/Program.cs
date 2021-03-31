4cf54f8e35931b762948ba1da7207cb18809cf56
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-01 02:10:03 +0300

    1.4.2021

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -7,9 +7,28 @@ namespace exercise_76
   {
     public static void Main(string[] args)
     {
+       List<int> numbers = new List<int>();
+       numbers.Add(3);
+       numbers.Add(2);
+       numbers.Add(6);
+       numbers.Add(-1);
+       Console.WriteLine(Sum(numbers));
 
+       numbers.Add(5);
+       numbers.Add(1);
+       Console.WriteLine(Sum(numbers));
+
+            Console.ReadKey();
     }
-
+        public static int Sum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach(int num in numbers)
+            {
+                sum = sum + num;
+            }
+            return sum;
+        }
   }
 }
 
