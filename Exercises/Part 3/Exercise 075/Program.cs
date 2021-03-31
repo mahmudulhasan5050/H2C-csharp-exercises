5a02b3604d006a8b82a5acb6ed00e4f43df2423b
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-01 02:02:46 +0300

    1.4.2021

--- Exercises/Part 3/Exercise 075/Program.cs
+++ Exercises/Part 3/Exercise 075/Program.cs
@@ -13,14 +13,34 @@ namespace exercise_75
       // List<int> numbers = new List<int>();
       // numbers.Add(5);
 
+      List<int> numbers = new List<int>();
+            numbers.Add(3);
+            numbers.Add(2);
+            numbers.Add(6);
+            numbers.Add(-1);
+            numbers.Add(5);
+            numbers.Add(1);
+
       Console.WriteLine("The numbers in the range [0, 5]");
       PrintNumbersInRange(numbers, 0, 5);
 
       Console.WriteLine("The numbers in the range [3, 10]");
       PrintNumbersInRange(numbers, 3, 10);
-    }
+
+            Console.ReadKey();
 
     }
+         public static void PrintNumbersInRange(List<int> numbers, int lowerLimit, int upperLimit)
+        {
+            foreach(int num in numbers)
+            {
+                if(lowerLimit<= num && upperLimit >= num)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            
+        }
   }
 }
 
