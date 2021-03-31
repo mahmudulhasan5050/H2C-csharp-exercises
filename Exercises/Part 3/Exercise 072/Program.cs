4fa26e7091a51fa5e09f0006f69edb6580ee4414
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-31 03:08:13 +0300

    31.3.2021

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -7,7 +7,37 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+            List<int> numbers = new List<int>();
 
+            while (true)
+            {
+                Console.Write("> ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                if(input == 9999)
+                {
+                    break;
+                }
+                numbers.Add(input);
+            }
+
+            int smallest = numbers[0];
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if(numbers[i] < smallest)
+                {
+                    smallest = numbers[i];
+                }
+            }
+            Console.WriteLine("Smallest number: " + smallest);
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if(numbers[i] == smallest)
+                {
+                    Console.WriteLine("Found at index: " + i);
+                }
+            }
+            Console.ReadKey();
     }
   }
 }
