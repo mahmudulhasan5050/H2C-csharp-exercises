5bbb8d01763c9a1fa033b359748d0ce73496fad6
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-03-31 03:18:01 +0300

    31.3.2021

--- Exercises/Part 3/Exercise 073/Program.cs
+++ Exercises/Part 3/Exercise 073/Program.cs
@@ -10,6 +10,7 @@ namespace exercise_73
       List<int> list = new List<int>();
       while (true)
       {
+        Console.Write("> ");
         int input = Convert.ToInt32(Console.ReadLine());
         if (input == -1)
         {
@@ -18,6 +19,13 @@ namespace exercise_73
         list.Add(input);
       }
 
+      int sum = 0;
+            for(int i = 0; i < list.Count; i++)
+            {
+                sum = sum + list[i];
+            }
+            Console.WriteLine("Sum: " + sum);
+            Console.ReadKey();
     }
   }
 }
