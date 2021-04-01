57319312e72d431950491ad2aa19f8da4b228128
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-01 05:01:11 +0300

    1.4.2021

--- Exercises/Part 3/Exercise 085/Program.cs
+++ Exercises/Part 3/Exercise 085/Program.cs
@@ -7,8 +7,25 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+            Console.Write("Enter username: \n> ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: \n> ");
+            string password = Console.ReadLine();
 
+            if(username == "alex" && password == "sunshine")
+            {
+                Console.WriteLine("You have successfully logged in!");
+            }
+            else if(username == "emma" && password == "haskell")
+            {
+                Console.WriteLine("You have successfully logged in!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username or password!");
+            }
 
+        Console.ReadKey();
     }
   }
 }
