1ab1ddd511413cf8c0cf00758cf524362aae9ae1
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-01 04:03:34 +0300

    1.4.2021

--- Exercises/Part 3/Exercise 081/Program.cs
+++ Exercises/Part 3/Exercise 081/Program.cs
@@ -7,13 +7,26 @@ namespace exercise_81
   {
     public static void Main(string[] args)
     {
+      int[] array = {5, 1, 3, 4, 2};
       // You can test your method here
+      PrintNeatly(array);
 
+            Console.ReadKey();
     }
 
     public static void PrintNeatly(int[] array)
     {
-
+        for(int i = 0; i < array.Length; i++)
+            {
+                if( i != array.Length - 1)
+                {
+                    Console.Write(array[i] + ", ");
+                }
+                else
+                {
+                    Console.Write(array[i]);
+                }
+            }
     }
   }
 }
