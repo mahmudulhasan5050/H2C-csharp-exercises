fde2626f930db70a1faeb36c4b9211a7079d78be
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-03 17:20:26 +0300

    3.4.2021

--- Exercises/Part 3/Exercise 086/Program.cs
+++ Exercises/Part 3/Exercise 086/Program.cs
@@ -7,8 +7,29 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
+            bool start = true;
+            
 
+            while (start) { 
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            string[] word = input.Split(new string[] {" "}, StringSplitOptions.None);
+            
 
+            if(input != "") 
+                {                      
+                  for(int i = 0; i < word.Length; i++) 
+                    { 
+                    Console.WriteLine(word[i]);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+        }
+            start = false;
+            Console.ReadKey();
     }
   }
 }
