f5b5159970db3bfaf90e34dc4d938f73d881de56
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-03 18:02:31 +0300

    3.4.2021

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -8,6 +8,32 @@ namespace exercise_87
     public static void Main(string[] args)
     {
 
+            bool start = true;
+            
+
+            while (start) { 
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            string[] word = input.Split(new string[] {" "}, StringSplitOptions.None);
+            
+
+            if(input != "") 
+                {                      
+                  for(int i = 0; i < word.Length; i++) 
+                    {
+                        if (word[i].Contains("av"))
+                        {
+                            Console.WriteLine(word[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+        }
+            start = false;
+            Console.ReadKey();
     }
 
   }
