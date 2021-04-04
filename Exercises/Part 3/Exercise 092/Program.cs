08fd5687d05c46d8e6e5029e447639113724e7e7
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-04 12:06:48 +0300

    4.4.2021

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,7 +7,47 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      List<string> lists = new List<string>();
 
+      while (true)
+         {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+             if (input == "")
+             {
+              break;
+             }
+
+             lists.Add(input);
+            
+         }
+
+      int ageHighest = 0;
+      string name = "";
+      int lengthName = 0;
+      int currentYear = 2020;
+
+      for(int i = 0; i < lists.Count; i++)
+            {
+               string[] eachInfo = lists[i].Split(new string[] {","}, StringSplitOptions.None);
+                int eachInfoYear = Convert.ToInt32(eachInfo[1]);
+                string eachInfoName = eachInfo[0];
+                int age = currentYear - eachInfoYear;
+
+                if(age > ageHighest)
+                {                   
+                    ageHighest = age;
+                }
+                if(eachInfoName.Length > lengthName)
+                {
+                    lengthName = eachInfoName.Length;
+                    name = eachInfoName;
+                }
+                
+            }
+      Console.WriteLine("Longest name: " + name);   //If name lenght is same, it will show only the first one.
+      Console.WriteLine("Highest age: " + ageHighest);
+      Console.ReadKey();
     }
   }
 }
