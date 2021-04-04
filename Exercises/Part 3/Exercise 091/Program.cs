359e239ac9defb0fd905738c2e4a1e8d9bb84f20
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-04 11:37:12 +0300

    4.4.2021

--- Exercises/Part 3/Exercise 091/Program.cs
+++ Exercises/Part 3/Exercise 091/Program.cs
@@ -7,7 +7,36 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
+      List<string> lists = new List<string>();
 
+      while (true)
+         {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+             if (input == "")
+             {
+              break;
+             }
+
+             lists.Add(input);
+            
+         }
+
+      int age = 0;
+      string name = "";
+      for(int i = 0; i < lists.Count; i++)
+            {
+               string[] eachInfo = lists[i].Split(new string[] {","}, StringSplitOptions.None);
+                int eachInfoAge = Convert.ToInt32(eachInfo[1]);
+                if(eachInfoAge > age)
+                {
+                    name = eachInfo[0];
+                    age = eachInfoAge;
+                }
+                
+            }
+      Console.WriteLine("Name of the oldest: " + name);
+      Console.ReadKey();
     }
   }
 }
