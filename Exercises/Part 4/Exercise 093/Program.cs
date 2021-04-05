47f730269087bb739c91c5b3f4e032728f64922b
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-06 02:56:13 +0300

    6.4.2021

--- Exercises/Part 4/Exercise 093/Program.cs
+++ Exercises/Part 4/Exercise 093/Program.cs
@@ -8,7 +8,17 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+            Account accountHasan = new Account("Hasan", 100.00);
 
+            Console.WriteLine("Intial state");
+            Console.WriteLine(accountHasan);
+
+            accountHasan.Deposit(20.0);
+
+            Console.WriteLine("End state");
+            Console.WriteLine(accountHasan);
+
+            Console.ReadKey();
     }
   }
 }
