6fc3bb3f4a27f64ecec3b7107d573f60d25eba81
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-07 02:17:19 +0300

    7.4.2021

---
+++ Exercises/Part 4/Exercise 098/Product.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace exercise_98
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+        private int quantity;
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine(this.name + ": price " + this.price + ": " + this.quantity + " pcs");
+        }
+    }
+}
