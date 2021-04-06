9a1befaf489f10feaa8eda14091cd77bf95501be
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-06 03:24:39 +0300

    6.4.2021

--- Exercises/Part 4/Exercise 094/Program.cs
+++ Exercises/Part 4/Exercise 094/Program.cs
@@ -8,7 +8,21 @@ namespace exercise_94
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+            Account heikkisAccount = new Account("Heikki's account", 1000.0);
+            Account personalAccount = new Account("Personal account", 0.0);
 
+            Console.WriteLine(heikkisAccount);
+            Console.WriteLine(personalAccount);
+
+            heikkisAccount.Withdrawal(100);
+            personalAccount.Deposit(100);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated info");
+            Console.WriteLine(heikkisAccount);
+            Console.WriteLine(personalAccount);
+
+            Console.ReadKey();
     }
   }
 }
