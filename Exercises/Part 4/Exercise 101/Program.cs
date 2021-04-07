71871d374e25283fe078f53eb81647d81e21db54
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-07 20:29:14 +0300

    7.4.2021

--- Exercises/Part 4/Exercise 101/Program.cs
+++ Exercises/Part 4/Exercise 101/Program.cs
@@ -8,6 +8,8 @@ namespace exercise_101
     {
       Dalmatian spotty = new Dalmatian("Spot", 306);
       Console.WriteLine(spotty.name + " is a very good dog. He has " + spotty.spots + " darker spots in his fur");
+
+            Console.ReadKey();
     }
   }
 }
