549be5bf5eaee3fa8cd6b582657ec6441fe91556
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-07 20:16:39 +0300

    7.4.2021

--- Exercises/Part 4/Exercise 100/Program.cs
+++ Exercises/Part 4/Exercise 100/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_100
   {
     public static void Main(string[] args)
     {
+            Debt mortgage = new Debt(120000.0, 1.01);
 
+            mortgage.PrintBalance();
+
+            mortgage.WaitOneYear();
+            mortgage.PrintBalance();
+
+              int years = 0;
+              while (years < 20)
+              {
+                  mortgage.WaitOneYear();
+                  years = years + 1;
+              }
+
+            mortgage.PrintBalance();
+
+
+            Console.ReadKey();
     }
   }
 }
