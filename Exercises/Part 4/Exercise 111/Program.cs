0c4365189ce2f412248e95c46b93b130e30dbc1c
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-09 17:51:16 +0300

    9.4.2021

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -10,6 +10,34 @@ namespace exercise_111
       // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
 
+            while (true)
+            {
+                Console.Write("First Name:\n> ");
+                string firstName = Console.ReadLine();
+                
+               
+                if(firstName == "")
+                {
+                    break;
+                }
+              
+                Console.Write("Last Name:\n> ");
+                string lastName = Console.ReadLine();
+                Console.Write("Identification number:\n> ");
+                string identificationNumber = Console.ReadLine();
+                    
+                
+            list.Add(new PersonalInformation(firstName, lastName, identificationNumber));
+
+            }
+
+            Console.WriteLine();
+            foreach(PersonalInformation li in list)
+            {
+                Console.WriteLine(li.firstName + " " + li.lastName);
+            }
+
+            Console.ReadKey();
     }
   }
 }
