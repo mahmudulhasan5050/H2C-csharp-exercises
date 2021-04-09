aed87a25cd99f64ffa30de9de0bdc44140dba19f
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-09 18:08:01 +0300

    9.4.2021

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -8,8 +8,41 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+      List<TelevisionProgram> television = new List<TelevisionProgram>();
 
 
+             while (true)
+            {
+                Console.Write("Name: ");
+                string programName = Console.ReadLine();
+                
+               
+                if(programName == "")
+                {
+                    break;
+                }
+              
+                Console.Write("Duration: ");
+                int duration = Convert.ToInt32(Console.ReadLine());
+                    
+                
+            television.Add(new TelevisionProgram(programName, duration));
+
+            }
+
+             Console.WriteLine();
+             Console.Write("Program's maximum duration? ");
+            int userDuration = Convert.ToInt32(Console.ReadLine());
+
+            foreach(TelevisionProgram tel in television)
+            {
+                if(tel.duration <= userDuration)
+                {
+                    Console.WriteLine(tel);
+                }
+            }
+
+             Console.ReadKey();
     }
   }
 }
