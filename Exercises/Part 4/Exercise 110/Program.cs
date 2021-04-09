2aae2d3260901bc70422e341833a517a04a2755b
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-09 17:17:14 +0300

    9.4.2021

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -7,7 +7,29 @@ namespace exercise_110
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<Item> names = new List<Item>();
+
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                if(name == "")
+                {
+                    break;
+                }
+                else
+                {
+                    names.Add(new Item(name));
+                }
+            }
+            
+            foreach(Item nam in names)
+            {
+                Console.WriteLine(nam);
+            }
+
+            Console.ReadKey();
     }
   }
 }
