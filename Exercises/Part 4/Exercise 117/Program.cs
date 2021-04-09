40c3042ba3fbbe0276af880f01ffae481b4aec8c
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-09 23:27:32 +0300

    9.4.2021

--- Exercises/Part 4/Exercise 117/Program.cs
+++ Exercises/Part 4/Exercise 117/Program.cs
@@ -9,7 +9,15 @@ namespace exercise_117
     {
       // Ask the user for the file name and print the content of the file
 
+          Console.Write("Which file should have its contents printed?\n> ");
+          string fileName = Console.ReadLine();
+
+
+        string startupPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Exercise 117", fileName);
+        string text = File.ReadAllText(startupPath);
       
+            Console.WriteLine(text);
+            Console.ReadKey();
     }
   }
 }
