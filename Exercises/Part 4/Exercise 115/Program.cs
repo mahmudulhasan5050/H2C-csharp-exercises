77ca36ce115fbede46042388c2c51e2697269ffc
Mahmudul Hasan <50146955+mahmudulhasan5050@users.noreply.github.com>
2021-04-09 20:28:54 +0300

    9.4.2021

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -6,7 +6,25 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
+             int resultCube = 0;
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                
+                if(input == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    int inputInt = Convert.ToInt32(input);
+                    resultCube = inputInt * inputInt * inputInt;
+                    Console.WriteLine(resultCube);
+                }
+            }
 
+            Console.ReadKey();
     }
   }
 }
